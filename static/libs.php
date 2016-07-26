0e85356580aba7f8a8b9ad8099bc0a834475d6a7
Adriano Ferraguto <0xADFE@gmail.com>
2016-07-26 04:51:49 +0200

    Remote libs replaced with local libs
    Once remote-located libs, are now included in the project for offline
    local server compatibility

--- static/libs.php
+++ static/libs.php
@@ -7,8 +7,15 @@
  */
 
 ?>
+
+<!--Remote libs
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+-->
 
-
+<!--Local libs-->
+<script src="../static/libs/jquery-3.1.0.min.js"></script>
+<link href="../static/libs/bootstrap.min.css" rel="stylesheet">
+<link href="../static/libs/bootstrap-glyphicons.css" rel="stylesheet">
+<script src="../static/libs/bootstrap.min.js"></script>
